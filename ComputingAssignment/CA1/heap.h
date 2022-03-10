966e61252c667c853fbdec0475bd66f6320f4e2e
Haoze Gao <haoze.20@intl.zju.edu.cn>
2022-03-10 22:00:29 +0800

    initial version of heap file

---
+++ ComputingAssignment/CA1/heap.h
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <string>
+using namespace std;
+
+// basic queue data structure
+//TODO:存在外部文件中还是内存中
+template<class T> class Data
+{
+public:
+    Data();
+    int timestamp[6]; // YY,MM,DD,HH,MM,SS
+
+private:
+    char id[8] = {"12345678"};//TODO:再说
+    string name;
+    Contact contact;
+    int profession; // I to VIII
+    long birth; // format: YYMMDD
+    int age_group;
+};
+
+template<class T> class Contact
+{
+private:
+    string address;
+    T phone;
+    T WeChat;
+    T email;
+}
+
+template<class T> class Local : public Data
+{
+public:
+    Local();
+    int update();   // update to central
+
+private:
+    T *queue;   // the queue of all local records
+
+}
+
+template<class T> class Central
+{
+public:
+    Central();
+    void Selection(); // select people with the higest priority
+    void Report();
+
+
+}
