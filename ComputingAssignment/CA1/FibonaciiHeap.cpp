dff0638aadca7732c8161dfd7a8d17391cd0e881
Haoze Gao <haoze.20@intl.zju.edu.cn>
2022-03-12 02:00:42 +0800

    list out essential attributes and functions

---
+++ ComputingAssignment/CA1/FibonaciiHeap.cpp
@@ -0,0 +1,37 @@
+#include "FibonaciiHeap.h"
+
+// insert new node into root
+template <class T>
+void Heap<T>::insert(Node<T> &node)
+{
+    // insert to root
+    Node *m;
+
+    // adjust H.min
+
+};
+
+// used to update information
+template <class T>
+void Heap<T>::decrease(Node<T> &node, Data<T> &new_data)
+{
+
+};
+
+template <class T>
+void Heap<T>::delete_node(Node<T> &node)
+{
+
+};
+
+template <class T>
+void Heap<T>::delete_min()
+{
+
+};
+
+template <class T>
+void Heap<T>::merge()
+{
+
+};
