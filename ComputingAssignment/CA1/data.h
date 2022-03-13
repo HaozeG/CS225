49271d5baa9ec657f353b76aed08f75153c0808a
Haoze Gao <haoze.20@intl.zju.edu.cn>
2022-03-13 14:20:34 +0800

    add ifndef check

--- ComputingAssignment/CA1/data.h
+++ ComputingAssignment/CA1/data.h
@@ -1,47 +1,56 @@
+#ifndef data_h
+#define data_h
+
 #include <stdint.h>
 #include <string>
 using namespace std;
 
+class Contact
+{
+public:
+    Contact();
+
+protected:
+    string address;
+    string phone;
+    string WeChat;
+    string email;
+};
+
 // basic queue data structure
 //TODO:存在外部文件中还是内存中
-template<class T> class Data
+class Data
 {
 public:
     Data();
 
 private:
     int timestamp[6]; // YY,MM,DD,HH,MM,SS
-    char id[8] = {"12345678"};  //TODO:再说
+    char id[9] = {"12345678"};  //TODO:再说
     string name;
-    Contact contact;
+    Contact *contact;
     int profession; // I to VIII
     long birth; // format: YYMMDD
     int age_group;
 };
 
-template<class T> class Contact
-{
-private:
-    string address;
-    T phone;
-    T WeChat;
-    T email;
-};
 
-template<class T> class Local : public Data
+class Local : public Data
 {
 public:
     Local();
     int update();   // update to central
 
 private:
-    T *queue;   // the queue of all local records
+    Data *queue;   // the queue of all local records
 };
 
-template<class T> class Central
+class Central : public Data
 {
 public:
     Central();
     void Selection(); // select people with the higest priority
     void Report();
 };
+
+#endif
