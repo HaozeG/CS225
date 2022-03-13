c117e360fc38bab9bf7d149e5306779735a6a247
Haoze Gao <haoze.20@intl.zju.edu.cn>
2022-03-13 14:23:45 +0800

    change include to avoid multiple definitions

---
+++ ComputingAssignment/CA1/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "fibonacii_heap.h"
+
+int main()
+{
+        // Data a;
+        // printf(a.id[0]);
+        // return 0;
+        Heap a;
+        printf("%d\n",a.n);
+        return 0;
+}
