6d2b2c594509bcbc53ce3c75d674a99df50a0184
Haoze Gao <804327645@qq.com>
2022-03-16 19:44:41 +0800

    add more functions and finish update, increase

--- ComputingAssignment/CA1/fibonacii_heap.cpp
+++ ComputingAssignment/CA1/fibonacii_heap.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 #include "fibonacii_heap.h"
 using std::cout;
 using std::cin;
@@ -17,46 +18,149 @@ Node::Node()
 
 Heap::Heap()
 {
-    min = NULL;
+    highest = NULL;
     n = 0;
+}
+
+void Heap::link_root(Node &node)
+{
+    // link to root list
+    Node *m = (*(Heap::highest)).right;
+    (*(Heap::highest)).right = &node;
+    node.left = Heap::highest;
+    node.right = m;
+    (*m).left = &node;
+
+    // adjust H.highest
+    if (higher_priority(node, *(Heap::highest)))
+        Heap::highest = &node;
 }
 
 // insert new node into root
 void Heap::insert(Node &node)
 {
-    // insert to root
-    Node *m = (*(Heap::min)).right;
-    (*(Heap::min)).right = &node;
-    node.left = Heap::min;
-    node.right = m;
-    (*m).left = &node;
-
-    // adjust H.min
-    //TODO: 优先级判断函数
-    if (is_smaller(node, *(Heap::min)))
-        Heap::min = &node;
+    link_root(node);
+    Heap::n++;
 };
 
 // used to update information
+void Heap::update(Node &node, Data &new_data)
+{
+    Node temp;
+    temp.data = &new_data;
+    // judge if the priority increases or decreases
+    bool flag = higher_priority(node, temp);
+    if (flag)
+    {
+        decrease(node, new_data);
+    }
+    else
+    {
+        increase(node, new_data);
+    }
+}
+
+void Heap::increase(Node &node, Data &new_data)
+{
+    // increase the data of the node
+    node.data = &new_data;
+
+    // update degree value of parent nodes
+    // mark parent node
+    // iterate through all parent nodes to check if they are marked
+    cascaded_cut(node);
+    update_degree(node.parent, node.degree);
+
+    // cut off subtree
+    // insert to root
+    node.parent = NULL;
+    link_root(node);
+}
+
 void Heap::decrease(Node &node, Data &new_data)
 {
+    // decrease the data of the node
+    node.data = &new_data;
 
+    // update degree value of parent nodes
+    // mark parent node
+    // iterate through all parent nodes to check if they are marked
+    cascaded_cut(node);
+    update_degree(node.parent, node.degree);
+
+    // update the link of parent node
+    if (&node == (node.parent)->child)
+        (node.parent)->child = node.left;
+    // cut off subtree
+    // insert to root
+    node.parent = NULL;
+    link_root(node);
+
+};
+
+void Heap::delete_highest()
+{
+    // check if the heap is empty
+    if (0 == Heap::n)
+    {
+        cout << "Already empty!\n";
+        return;
+    }
+    Heap::n--;
+    // connect child nodes to root list
+
+    // rebalance
+    consolidate();
 };
 
 void Heap::delete_node(Node &node)
 {
+    // set the value of new_data to negative
+    Data new_data;
+    Data *origin_data = node.data;
+
+    // call decrease and delete_min
+    decrease(node, new_data);
+    delete_highest();
+    // TODO: 是否需要修改原来的数据
+    // // keep the data unchanged
+    // node.data = origin_data;
+};
+
+void Heap::consolidate()
+{
+    Node *m[int(log2(n))];
+    // iterate through root list
+
+    // merge two trees if needed
+
+    // update H.highest
 
 };
 
-void Heap::delete_min()
+bool higher_priority(Node &node1, Node &node2)
 {
-};
+    // profession category, ranking of age group, time
 
-void Heap::merge()
-{
-
-};
-
-bool Heap::is_smaller(Node &node1, Node &node2)
-{
 }
+
+void Heap::update_degree(Node *node, int d)
+{
+    while (NULL != (*node).parent)
+    {
+        (*node).degree -= d;
+        node = (*node).parent;
+    }
+}
+
+        // //TODO:if逻辑
+        // if ((false == (*node).mark) && (false == flag))
+        // {
+        //     (*node).mark = true;
+        //     flag = false;
+        // }
+        // else
+        // {
+        //     // cut off node
+
+        // }
--- ComputingAssignment/CA1/fibonacii_heap.h
+++ ComputingAssignment/CA1/fibonacii_heap.h
@@ -7,12 +7,14 @@ class Node
 {
 public:
     Node();
+
+    Data* data;
     // define four pointers for each node
     Node* parent;
     Node* child;
     Node* left;
     Node* right;
-    int degree;
+    int degree; // number of children
     bool mark;
 };
 
@@ -20,15 +22,21 @@ class Heap : public Node
 {
 public:
     Heap();
-    Node *min;  // pointer to the smallest node
+    Node *highest;  // pointer to the highest priority node
     int n;  // record the number of nodes in this heap
 
-    bool is_smaller(Node &node1, Node &node2);
     void insert(Node &node);
-    void decrease(Node &node, Data &new_data);   // used to update information
+    void link_root(Node &node);
+    void update(Node &node, Data &new_data); // used to update information
+    void increase(Node &node, Data &new_data);
+    void decrease(Node &node, Data &new_data);
     void delete_node(Node &node);
-    void delete_min();
-    void merge();   // rebalance
+    void delete_highest();
+    void consolidate();
+    void update_degree(Node *node, int d);
+    void cascaded_cut(Node &node);
 };
+
+bool higher_priority(Node &node1, Node &node2);
 
 #endif
