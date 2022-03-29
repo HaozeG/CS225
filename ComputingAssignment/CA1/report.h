e162526ef1fc10d6ebbf6f2337bb7625d86fb748
ZYH <yihengz.20@intl.zju.edu.cn>
2022-03-29 20:17:34 +0800

    proces

--- ComputingAssignment/CA1/report.h
+++ ComputingAssignment/CA1/report.h
@@ -2,6 +2,7 @@
 #define report_h
 
 #include <ratio>
+#include <cstdlib>
 #include <stdint.h>
 #include <string>
 #include <time.h>
@@ -9,8 +10,15 @@
 #include "fibonacii_heap.h"
 using namespace std;
 
+
+class Brutal_node
+{
+public:
+    Brutal_node *next;
+    Data *ptr;
+};
 //used for reporting  process,strcmp()!!!!!!!, 用建构函数写数据的初始化
-class Report_system
+class Report_system : public Brutal_node
 {
 public:
     long Regi_number = 0;
@@ -21,31 +29,29 @@ public:
     //choose the order of weekly report
     int weekly_choice();
     //open file for weekly report
-    void open_file();
-    void Writing_weekly(Node *node, int choice);
+    void open_file(Data *data, int Choice);
+    void Writing_weekly(Data *data, int choice, int Choice_2);
     //open and write for monthly report
     void file_month();
     //count different category number for one node
     void stat(Node *node);
+    //copy list
+    Brutal_node *Copied_list(Data *data, Brutal_node *ptr);
     //choose which operation to use
     void selection(int number, int number_two, Heap &H);
+
+    
     //sort by name
-    void sort_by_name(Heap &H, int number);
+    void sort_by_name(Brutal_node *ptr, int number);
     //sort by profession
-    void sort_by_profession(Heap &H, int number);
+    void sort_by_profession(Brutal_node *ptr, int number);
     //sort by age
-    void sort_by_age(Heap &H, int number);
+    void sort_by_age(Brutal_node *ptr, int number);
 };
 //usde for counting time
 class Counting_system
 {
 public:
     time_t timep;  
-};
-
-class Brutal_node
-{
-public:
-    Brutal_node *next;
 };
 #endif
