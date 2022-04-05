e231ec7558e2d2055972274c6916fe5f0b2d30c4
Haoze Gao <804327645@qq.com>
2022-04-05 21:45:44 +0800

    add comments

--- ComputingAssignment/CA1/timeoffset.h
+++ ComputingAssignment/CA1/timeoffset.h
@@ -1,7 +1,7 @@
 #ifndef timeoffset_h
 #define timeoffset_h
 
-extern long timeoffset;
-extern long timestart;
+extern long timeoffset;         // hours
+extern long timestart;          // YYYYMMDD(valid for 20220401-20220531)
 
 #endif
