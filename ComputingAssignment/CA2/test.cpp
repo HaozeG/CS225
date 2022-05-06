2dd326778b430d30ad9cb8971a6ef51a178564d1
Haoze Gao <804327645@qq.com>
2022-05-06 14:16:41 +0800

    test

--- ComputingAssignment/CA2/test.cpp
+++ ComputingAssignment/CA2/test.cpp
@@ -1,37 +1,64 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstdlib>
-#include "data.h"
 #include <stdlib.h>
 #include <cstring>
+#include "data.h"
+#include "timeoffset.h"
+#include "bp_tree.h"
 using std::cin;
 using std::cout;
+long timeoffset = 0;
 
 int main()
 {
-    Local* abc = new Local();
+    bp_tree::Tree* bp_tree = new bp_tree::Tree;
+    Local* abc = new Local;
     const char* a = "testfile.txt";
-    abc->readfile(a); int i = 1;
+    abc->readfile(a);
+    cout << "test\n";
+    bp_tree->insert(abc->local->head->block[3]->key(), abc->local->head);
+    bp_tree->insert(abc->local->head->block[3]->key(), abc->local->head);
+    bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    bp_tree->insert(abc->local->head->block[3]->key(), abc->local->head);
+    bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    bp_tree->insert(abc->local->head->block[3]->key(), abc->local->head);
+    //     bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    //     bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    //     bp_tree->insert(abc->local->head->next->block[3]->key(), abc->local->head->next);
+    //     bp_tree->insert(abc->local->head->next->block[0]->key(), abc->local->head->next);
+    bp_tree->display(bp_tree->root_node);
+    int i = 1;
     const char* x = "3200000002";
     //abc->local->head->bdelete(x);
     //abc->local->merge(abc->local->head, abc->local->head->next);
     //Block<relation>* tem = abc->local->head;
     for (Block<relation>* tem = abc->local->head; tem != NULL; tem = tem->next)
-    {//cout<<(tem->next == NULL)<<"\n";
-        cout<<"Block"<<i<<"\n"; i++;
+    { //cout<<(tem->next == NULL)<<"\n";
+        cout << "Block" << i << "\n";
+        i++;
         for (int j = 0; j < tem->length; j++)
         {
-            if (tem->block[j] == NULL) continue;cout<<j<<"\n";
-            cout<<"relation"<<j<<"\n";
-            cout<<"person"<<" "<<tem->block[j]->person->id<<" "<<tem->block[j]->person->name
-                <<" "<<tem->block[j]->person->birth<<" "<<tem->block[j]->person->age_group
-                <<" "<<tem->block[j]->person->phone<<" "<<tem->block[j]->person->WeChat
-                <<" "<<tem->block[j]->person->email<<"\n";
-            cout<<"status"<<" "<<tem->block[j]->status->risk<<" "<<tem->block[j]->status->priority
-                <<" "<<tem->block[j]->status->type<<"\n";
-            cout<<"registration"<<" "<<tem->block[j]->registration->timestamp<<"\n";
-            cout<<"treatment"<<" "<<tem->block[j]->treatment->time<<" "<<tem->block[j]->treatment->hospital_id<<"\n";
-            cout<<"\n";
+            if (tem->block[j] == NULL)
+                continue;
+            cout << j << "\n";
+            cout << "relation" << j << "\n";
+            cout << "person"
+                 << " " << tem->block[j]->person->id << " " << tem->block[j]->person->name
+                 << " " << tem->block[j]->person->birth << " " << tem->block[j]->person->age_group
+                 << " " << tem->block[j]->person->phone << " " << tem->block[j]->person->WeChat
+                 << " " << tem->block[j]->person->email << "\n";
+            cout << "status"
+                 << " " << tem->block[j]->status->risk << " " << tem->block[j]->status->priority
+                 << " " << tem->block[j]->status->type << "\n";
+            cout << "registration"
+                 << " " << tem->block[j]->registration->timestamp << "\n";
+            cout << "treatment"
+                 << " " << tem->block[j]->treatment->time << " " << tem->block[j]->treatment->hospital_id << "\n";
+            cout << "\n";
         }
         //if (tem->retrieval(x) != NULL)
         //cout<<"\n"<<tem->retrieval(x)->person->id<<"\n";
