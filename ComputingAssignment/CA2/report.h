30b2267c3a174805130013cb93d04f9d60062d83
Haoze Gao <804327645@qq.com>
2022-05-19 19:47:58 +0800

    merge

--- ComputingAssignment/CA2/report.h
+++ ComputingAssignment/CA2/report.h
@@ -17,7 +17,7 @@ public:
     void Week(blist<relation>* ptr, int Choice, int Choice_2, long timeoffset, bool treating);
     void Month(blist<relation>* ptr, long timeoffset);
     relation* Sorting(blist<relation>* head, int Choice_2, int Choice);
-    void time_calculator(bool treating, relation *paste, long timeoffset);
+    void time_calculator(bool treating, relation* paste, long timeoffset);
     static bool cmp_age(relation a, relation b);
     static bool cmp_profession(relation a, relation b);
     static bool cmp_name(relation a, relation b);
